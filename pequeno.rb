6984bed06df5db81cc146a8a30a623e14a99707c
Joaquim Adráz <joaquim.adraz@gmail.com>
2013-11-25 08:44:18 +0000

    bundler is up and running, write your gems in gemfile, bundler will require them

--- pequeno.rb
+++ pequeno.rb
@@ -1,10 +1,4 @@
-require 'rubygems'
-require 'bundler' 
-require 'sinatra'
-require "sinatra/json"
-
 class Pequeno < Sinatra::Application
-	# Bundler.require 
 end
 
 require_relative 'routes/init'
